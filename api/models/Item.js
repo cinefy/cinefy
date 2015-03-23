0c3509d46569db9af0e50688fc48ddca5488395c
callumsong <ghksong@gmail.com>
2015-03-22 19:51:10 -0700

    small change made to schema

--- api/models/Item.js
+++ api/models/Item.js
@@ -8,7 +8,7 @@ module.exports = function(sequelize) {
     picture: Sequelize.STRING,
     item_type: Sequelize.STRING,
     item_name: Sequelize.STRING,
-    time_in_movie: Sequelize.NUMBER
+    time_in_movie: Sequelize.INTEGER
   });
 
   return Item;
