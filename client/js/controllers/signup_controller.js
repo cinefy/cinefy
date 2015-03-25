a96a74afb58cae8146536401c0fb406a2b2ccc82
Sean Golob <seangolob@live.com>
2015-03-25 12:37:06 -0700

    signupin controller

--- client/js/controllers/signup_controller.js
+++ client/js/controllers/signup_controller.js
@@ -1,17 +1,31 @@
 'use strict';
 
 module.exports = function(app) {
-  app.controller('signupinController', ['$scope', '$cookies', '$location', '$http', function($scope, $cookies, $location, $http) {
+  app.controller('signupinController', ['$scope', '$cookies', '$location', '$http', '$base64', function($scope, $cookies, $location, $http, $base64) {
 
-    if($cookies.eat || $cookies.eat.length > 0) {
+    if($cookies.eat.length > 0) {
       $location.path('/profile');
     }
 
+    $scope.showLogin = function() {
+      var login = document.getElementById('login');
+      var signup = document.getElementById('signup');
+
+    }
+
+    $scope.showSignup = function() {
+
+    }
+
     $scope.signUp = function() {
-      $http.post('/api/v1/create_user', {
-        {name: data-ng-model-signup, password: data-ng-model-password}
+      $http({
+        method: 'POST',
+        url: '/api/v1/signup',
+        data: {name: $scope.signup.username, password: $scope.signup.password}
       })
-      .success(function() {
+      .success(function(data) {
+        console.log('user successfully created');
+        $cookies.eat = data.eat;
         $location.path('/profile');
       })
       .error(function(data) {
@@ -20,11 +34,11 @@ module.exports = function(app) {
     };
 
     $scope.signIn = function() {
-      $http({
-        method: 'GET',
-        url: 'api/v1/sign_in'
-      })
-      .success(function() {
+      $http.defaults.headers.common['Authorization'] = 'Basic: ' + $base64.encode($scope.login.username + ':' + $scope.login.password);
+      $http.get('/api/v1/sign_in')
+      .success(function(data) {
+        console.log('logged in');
+        $cookies.eat = data.eat;
         $location.path('/profile');
       })
       .error(function(data) {
