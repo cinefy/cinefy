977ab3f276f06e0c5ce6ff34aeb3ff585b78716d
Brandon Burns <brandon10688@gmail.com>
2015-03-25 13:01:37 -0700

    current

--- client/js/controllers/profile_controller.js
+++ client/js/controllers/profile_controller.js
@@ -4,15 +4,15 @@ module.exports = function(app) {
   app.controller('profileController', ['$scope', '$http', 
     function($scope, $http) {
 
-    $scope.get_likes = [];
+    $scope.allLikes = [];
 
-    $scope.get = function(data) {
+    $scope.getLikes = function(name) {
       $http({
         method: 'GET',
-        url: '/api/v1/get_likes';
+        url: '/api/v1/get_likes/'+ name;
       })
       .success(function(data) {
-        $scope.get_likes = data;
+        $scope.allLikes = data;
       })
       .error(function(data) {
         console.log(data);
