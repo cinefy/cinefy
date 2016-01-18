0e9785d0e3c9eac9641535d314c987d66db1f1f3
callumsong <ghksong@gmail.com>
2016-01-18 19:27:35 +0000

    added upgraded angular and package json files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-browserify');
 
   grunt.initConfig({
     clean: {
